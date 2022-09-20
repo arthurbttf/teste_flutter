7cfbd0e0a54f0cfc2231b59133cef431c2fa994e
arthurbttf <arthurbttf@gmail.com>
2022-09-20 15:52:31 -0300

    a

---
+++ test/page_repository_test.dart
@@ -0,0 +1,8 @@
+import 'package:dio/dio.dart';
+import 'package:exemplo/app/modules/posts/repositories/repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class DioMock extends Mock implements Dio {}
+
+void main() {}
